1cfe57564ecb3dddf61552b9bb139487e38319f3
Mike Dietz <mikedietz@hotmail.com>
2019-07-04 21:28:17 +0200

    restored files to original state due to error message

--- meetup/src/App.js
+++ meetup/src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>World!</code>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
