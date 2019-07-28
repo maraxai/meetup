8b415bc281087387e9419a3667cf02f710c35031
Mike Dietz <mikedietz@hotmail.com>
2019-07-28 21:35:10 +0200

    Updates

--- precache-manifest.f2f12ca149b0adb6d27eb25721f57f5a.js
+++ precache-manifest.f2f12ca149b0adb6d27eb25721f57f5a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5820a2ef979edb71046c91fddf732626",
+    "revision": "078aaf3761d05b3ac38bde51fdc1c7ca",
     "url": "/meetup/index.html"
   },
   {
-    "revision": "2ca3702fe67517ba0db9",
+    "revision": "484960da3c975ad05501",
     "url": "/meetup/static/css/main.f0c51ac9.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meetup/static/js/2.db0827aa.chunk.js"
   },
   {
-    "revision": "2ca3702fe67517ba0db9",
-    "url": "/meetup/static/js/main.a1b5d4d2.chunk.js"
+    "revision": "484960da3c975ad05501",
+    "url": "/meetup/static/js/main.d3fdadb5.chunk.js"
   },
   {
     "revision": "c81de03e5fcc2eb84eed",
